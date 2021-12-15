5f449bec9e2d4faa63f4e01368ff0add9c66817a
Hafiz Faruqi <faruqihafiz21@gmail.com>
2021-12-15 19:18:44 +0700

    commit sort module

---
+++ Modul5/F_SOAL5_FARUQI_1202204105.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import time
+
+Algorithm = time.time()
+
+class BubbleSort:
+    def __init__(self, filename, collumn:int):
+        self.filename = filename
+        self.collumn = collumn
+
+    def Sort(self):
+        file = pd.read_csv(self.filename)
+        file = file.values.tolist()
+
+        if self.collumn == 0:
+            print('Sorted by Rank')
+        elif self.collumn == 1:
+            print('Sorted By Title')
+        elif self.collumn == 2:
+            print('Sorteed by H-5 Index')
+        elif self.collumn == 3:
+            print('Sorteed by H-5 Median')
+
+        for i in range(len(file)):
+            swapped = False
+            for j in range(0,len(file)-i-1):
+                if file[j][self.collumn] > file[j + 1][self.collumn]:
+                    file[j], file[j + 1] = file[j + 1], file[j]
+                    swapped = True
+            if swapped == False:
+                break
+               
+        for sort in range(10):
+            print(file[sort])
+
+        print(f'Time taken to run: {time.time() - Algorithm} seconds')
+
+if __name__ == '__main__':
+    Files = BubbleSort('Daspro.csv',3)
+    Files.Sort()
+  
+
+
+
+    
